e1228027a4ba51c07ed96429a3fc141e66476ec4
Ehelsan <ehelsan@ehelsans-air.home>
2025-06-18 20:47:54 +0200

    made design changes in css file to make the website more responsive

--- src/data.js
+++ src/data.js
@@ -1,13 +1,24 @@
 export default [
   {
     id: 1,
-    src:"src/assets/moucha.jpg",
-    alt:"image of location",
+    src:"src/assets/mouchaisland.jpg",
+    alt:"image of moucha island",
     markerImg: "src/assets/location-marker.png",
     country: "Djibouti",
     mapslink: "https://maps.app.goo.gl/EezsBUTTASs7wKLHA",
     locationName: "Moucha Island",
     date: "November 2024",
     text: "My first visit to a 'real' island. The U.K. is not what comes to mind when you envision an island is it? Spent 2 days in this idyllic tropical paradise whilst visiting some family. But watch out for sharks!"
+  },
+  {
+    id: 2,
+    src:"src/assets/kln.jpg",
+    alt:"image of cologne cathedral",
+    markerImg: "src/assets/location-marker.png",
+    country: "Germany",
+    mapslink: "https://maps.app.goo.gl/EezsBUTTASs7wKLHA",
+    locationName: "Cologne Cathedral",
+    date: "February 2024",
+    text: "Had the best döner kebab of my life here. Kudret Kebap House in the Ehrenfield region. The stuff dreams are made of."
   }
 ]
