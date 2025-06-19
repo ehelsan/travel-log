871d141523faa3c6716819ee1f3a0d1630ecbad7
Ehelsan <ehelsan@ehelsans-air.home>
2025-06-19 16:00:54 +0200

    added more destinations to the travel log

--- src/data.js
+++ src/data.js
@@ -16,9 +16,44 @@ export default [
     alt:"image of cologne cathedral",
     markerImg: "src/assets/location-marker.png",
     country: "Germany",
-    mapslink: "https://maps.app.goo.gl/EezsBUTTASs7wKLHA",
+    mapslink: "https://maps.app.goo.gl/Lf5nHiqzewAaDwW29",
     locationName: "Cologne Cathedral",
     date: "February 2024",
-    text: "Had the best döner kebab of my life here. Kudret Kebap House in the Ehrenfield region. The stuff dreams are made of."
-  }
+    text: "Had the absolute best döner kebab and baklava of my life here. Kudret Kebap House in the Ehrenfield region. The stuff dreams are made of."
+  },
+  {
+    id: 3,
+    src:"src/assets/banff.jpg",
+    alt:"image of banff national park",
+    markerImg: "src/assets/location-marker.png",
+    country: "Canada",
+    mapslink: "https://maps.app.goo.gl/m5zxR19tqpSNEa6n7",
+    locationName: "Banff National Park",
+    date: "July 2023",
+    text: "Went on a road trip on my birthday to this gorgeous national park in the Alberta province of Canada. Shock blue water and the most stunning forests you will ever see."
+  },
+  {
+    id: 4,
+    src:"src/assets/amsterdam.jpg",
+    alt:"image of tenements on the amstel river",
+    markerImg: "src/assets/location-marker.png",
+    country: "Netherlands",
+    mapslink: "https://maps.app.goo.gl/c7WhXVLXyT63g6TAA",
+    locationName: "Amsterdam Canals",
+    date: "January 2023",
+    text: "Although I've been to the Netherlands countless times before this, and also lived and grew up an hour away as a child, this was my first visit to Amsterdam. The picturesque canals and beautiful architecture did not disappoint."
+  },
+  {
+    id: 5,
+    src:"src/assets/alanya.jpg",
+    alt:"image of kleopatra beach",
+    markerImg: "src/assets/location-marker.png",
+    country: "Turkey",
+    mapslink: "https://maps.app.goo.gl/hqFR1isJiZiFXtTHA",
+    locationName: "Kleopatra Beach",
+    date: "July 2022",
+    text: "The most beautiful beach I have ever been too. Had a gruelling routine of hiking up the mountain for 2 hours every morning with my Dad in the scorching heat. Resting at this gorgeous beach after the hike definitely made up for it."
+  },
+
+
 ]
